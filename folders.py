e46cbb118fef1c3d7cb3f0c109706ad428637790
Денчик <98116130+denischagin@users.noreply.github.com>
2022-01-31 17:58:03 +0300

    Денис

--- folders.py
+++ folders.py
@@ -5,8 +5,14 @@ def path():
   pass
 
 
-def dictionary():
-  pass
+def dictionary(way, a={}):
+    for filename in os.listdir(way):
+        if os.path.isdir(way + '\\' + filename):
+            dictionary(way + '\\' + filename)
+        else:
+            a.update({way + '\\' + filename: os.path.getsize(way + '\\' + filename)})
+    return a
+  
 
 
 def duplicate():
